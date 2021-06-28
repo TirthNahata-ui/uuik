af73ecdfb2adffbfea9b2be9dec47b1932974e5f
TirthNahata-ui <85680126+TirthNahata-ui@users.noreply.github.com>
2021-06-28 16:37:28 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,46 @@
+class Player
+{
+    constructor() {
+		this.x = 500;
+		this.y=200;
+		this.spt=createSprite(this.x, this.y, 100,100);
+		this.spt.shapeColor="blue";
+
+	}
+
+  applyGravity() {
+       this.spt.velocityY=this.spt.velocityY+2;
+	} 
+
+	moveRight(){
+		this.spt.x= this.spt.x+5
+	}
+	moveLeft(){
+		this.spt.x= this.spt.x-5
+	}
+    moveUp(){
+		this.spt.velocityY= -12
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
